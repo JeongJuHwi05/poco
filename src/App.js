4809679fbede38b5fa33513a3dc9500001416502
juhwi <sohe2012@naver.com>
2023-08-07 18:21:31 +0900

    2023.08.07/메뉴 완, home.js 수정/정주휘

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import './reset.css';
+
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import { useEffect,useState } from 'react';
+
+import { db } from './firebase';
+import { collection, getDocs, doc } from '@firebase/firestore';
 
 import Home from "./component/Home";
 import HoHold from "./component/HoHold";
@@ -9,15 +14,55 @@ import SpenPatt from "./component/SpenPatt";
 import Fixed from "./component/Fixed";
   
 function App() {
+  // 데이터베이스 연결 객체 생성
+  const pocoCollectionRef = collection(db, 'poco')
+  console.log(pocoCollectionRef);
+
+  useEffect( () => {
+    // async 사용해서 비동기 식으로 사용
+    const getList = async () => {
+      // getDocs(DB 연결객체)로 데이터 가져오기
+      // query(DB 연결객체, orderBy("기준열","정렬 방식"))
+
+      const data = await getDocs(
+        pocoCollectionRef
+      )
+    }
+
+    getList();
+  },[])
+
 
   // 크기 여부를 확인하는 상태 변수
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
-  const isDesktop = useMediaQuery({ minWidth: 1024 });
 
   // 스마트폰 크기 일 때 이미지 경로를 동적으로 설정
-  const logoImage = isMobile ? '/image/mLogo.png' : '/image/wLogo.png';
+  const logoImage = isMobile ? '/image/logo/mLogo.png' : '/image/logo/wLogo.png';
 
+  // 사이드 메뉴 초기값 false로 설정
+  const [isSideMenuOpen, setSideMenuOpen] = useState(false);
+
+  // 사이드 메뉴 이벤트 함수
+  const toggleSideMenu = () => {
+    setSideMenuOpen(!isSideMenuOpen);
+  };
+  
+  // 스마트폰 크기 일 때 메뉴 아이콘 경로를 동적으로 설정
+  const menuIcon = isSideMenuOpen ? '/image/icon/xIcon.png' : '/image/icon/menuIcon.png';
+
+  // 현재 활성화된 버튼의 값을 저장하는 상태 변수
+  const [activeButton, setActiveButton] = useState(null);
+
+  // 클릭한 버튼에 스타일을 적용하는 함수
+  const applyButtonStyle = (buttonValue) => {
+    if (buttonValue === '/') {
+      return buttonValue = '';
+    } else if (buttonValue.substr(0, 1) === 'm') {
+      return activeButton === buttonValue.substr(1) ? 'activeMBtn' : '';
+    } else {
+      return activeButton === buttonValue ? 'activeBtn' : '';
+    }
+  };
 
   return (
     <BrowserRouter>
@@ -25,13 +70,52 @@ function App() {
         <header>
           <nav>
             <div className='topMenu'>
-              <NavLink to='/'><img src={logoImage} alt="Logo" /></NavLink>
+              <NavLink to='/' className={`navLinkButton ${applyButtonStyle('/')}`} onClick={() => setActiveButton('/')}>
+                  <img className='mainLogo' src={logoImage} alt="Logo" />
+              </NavLink>
 
-              <ul>
-                <li><button type='button'><NavLink to='/HoHold' style={{ textDecoration: "none" }}>가계부</NavLink></button></li>
-                <li><button type='button'><NavLink to='/SpenPatt' style={{ textDecoration: "none" }}>소비패턴</NavLink></button></li>
-                <li><button type='button'><NavLink to='/Fixed' style={{ textDecoration: "none" }}>고정 지출입</NavLink></button></li>
-              </ul>
+              {isMobile ?
+                (
+                  <div>
+                    <button className={`sideMenuToggle ${isSideMenuOpen ? 'active' : ''}`} onClick={toggleSideMenu}>
+                      <img src={menuIcon} alt='Menu'/>
+                    </button>
+                    <div className={`sideMenu ${isSideMenuOpen ? 'active' : ''}`}>
+                      <ul>
+                        <li><NavLink to='/HoHold' className={`navLinkMButton ${applyButtonStyle('mHoHold')}`} onClick={() => setActiveButton('HoHold')}>가계부</NavLink></li>
+                        <li><NavLink to='/SpenPatt' className={`navLinkMButton ${applyButtonStyle('mSpenPatt')}`} onClick={() => setActiveButton('SpenPatt')}>소비패턴</NavLink></li>
+                        <li><NavLink to='/Fixed' className={`navLinkMButton ${applyButtonStyle('mFixed')}`} onClick={() => setActiveButton('Fixed')}>고정 지출입</NavLink></li>
+                      </ul>
+                    </div>
+                  </div>
+                )
+                :
+                (
+                  <ul>
+                    <li>
+                      <NavLink to='/HoHold'>
+                        <button className={`navLinkButton ${applyButtonStyle('HoHold')}`} type='button' onClick={() => setActiveButton('HoHold')}>
+                          가계부
+                        </button>
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to='/SpenPatt'>
+                        <button className={`navLinkButton ${applyButtonStyle('SpenPatt')}`} type='button' onClick={() => setActiveButton('SpenPatt')}>
+                          소비패턴
+                        </button>
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to='/Fixed'>
+                        <button className={`navLinkButton ${applyButtonStyle('Fixed')}`} type='button' onClick={() => setActiveButton('Fixed')}>
+                          고정 지출입
+                        </button>
+                      </NavLink>
+                    </li>
+                  </ul>
+                )
+              }
             </div>
           </nav>
         </header>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import '../css/Home.css';
+import '../reset.css';
+
+import React, { useState } from 'react';
 
 function Home() {
+  // 새로운 소비 내역 추가 시 지출입 input(combobox)
+  const [selectedValue, setSelectedValue] = useState('');
+  const [options] = useState(['수입', '지출']);
+
+  const handleInputChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedValue(option);
+  };
+
+
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='homeApp'>
+      <div className='contentDiv'>
+        <h3 className='subTitle'>새로운 소비 내역 추가하기</h3>
+        <div className='newInput'>
+          <ul>
+            <li>지출입</li>
+            <li>내역</li>
+            <li>금액</li>
+          </ul>
+
+          <ul>
+            <input type="text" value={selectedValue} onChange={handleInputChange} placeholder="지출입 콤보박스"></input>
+            {options.map((option, index) => (
+              <li key={index} onClick={() => handleOptionClick(option)}>
+                {option}
+              </li>
+            ))}
+            <li><input type="text"value={selectedValue} onChange={handleInputChange} placeholder="내역 입력 inputbox"></input></li>
+            <li><input type="text"value={selectedValue} onChange={handleInputChange} placeholder="금액 입력 inputbox"></input></li>
+          </ul>
+
+          <ul>
+            <li>날짜</li>
+            <li>태그</li>
+          </ul>
+
+          <ul>
+            <li><input type="text"value={selectedValue} onChange={handleInputChange} placeholder="날짜 입력 폼"></input></li>
+            <li><input type="text"value={selectedValue} onChange={handleInputChange} placeholder="태그 입력 inputbox"></input></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='contentDiv'>
+        <h3>이번달 자산 현황</h3>
+        <h2 className='subTitle'>이번달 전체 금액</h2>
+
+        <div className='assetBorder'>
+          <ul>
+            <li>수입</li>
+            <li>지출</li>
+          </ul>
+
+          <ul>
+            <li>수입 금액</li>
+            <li>지출 금액</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <h3 className='subTitle'>최근 내역</h3>
+        <div className='lastList'>
+
+        </div>
+
+        <button className='moreBtn' type='button'>더보기</button>
+      </div>
     </div>
   );
 }
